8712d606e063c6a4b396b1f492c825c65d38bb1b
Kit Pyles <41458467+MamaKit@users.noreply.github.com>
2022-08-20 20:28:40 -0500

    Update scriptHelper.js
    Try to fix

--- scriptHelper.js
+++ scriptHelper.js
@@ -86,10 +86,11 @@ async function myFetch() {
     let planetsReturned;
 
     planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response) {
-        response.json().then( function(json) {
-            console.log(json);
-            return json;
-         });
+        response.json()
+//             .then( function(json) {
+            console.log(response.json);
+//             return json;
+//          });
     });
 
     return planetsReturned;
